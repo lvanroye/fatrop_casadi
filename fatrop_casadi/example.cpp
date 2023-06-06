323e0f484e89f31647573a8ac8f70a89e7e3cce2
landerv <lander.vanroye@kuleuven.be>
2023-06-06 16:31:08 +0200

    wip

---
+++ fatrop_casadi/example.cpp
@@ -0,0 +1,17 @@
+#include "single_stage.hpp"
+#include <casadi/casadi.hpp>
+
+int main()
+{
+    // create a simple test opti problem
+    casadi::Opti opti;
+    // add x variables to opti
+    auto x =  opti.variable(1);
+    // set objective
+    opti.minimize(x*x);
+    // create solver instance
+    opti.solver("ipopt");
+    // solve the problem
+    auto sol = opti.solve();
+    return 0;
+}
